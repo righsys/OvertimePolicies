f64b0232928b96a05ed96bddaf32a972429f5900
Mhasani <Mhasani@MHASANI-SURFACE>
2022-12-09 23:47:06 +0330

    First Command Test

--- OvertimePolicies.Services.Tests/Employee/Commands/AddEmployeeCommandTests.cs
+++ OvertimePolicies.Services.Tests/Employee/Commands/AddEmployeeCommandTests.cs
@@ -8,14 +8,18 @@ using OvertimePolicies.Services.Interfaces.EFCoreRepositories;
 using OvertimePolicies.Services.Interfaces;
 using OvertimePolicies.WebApp.Common.DatetimeHelper;
 using Microsoft.Extensions.Logging;
+using OvertimePolicies.Domain.Events;
 
 namespace OvertimePolicies.Services.Tests.Employee.Commands
 {
     public class AddEmployeeCommandTests:CommandTestBase<AddEmployeeCommand>
     {
-        public AddEmployeeCommandTests(IEFCoreEmployeeRepository employeeRepository, 
-            ICurrentUserService currentUserService, 
-            IDateTimeHelper dateTimeHelper, ILogger<AddEmployeeCommand> logger) : base(employeeRepository, currentUserService, dateTimeHelper,logger)
+        public AddEmployeeCommandTests(
+                IEFCoreEmployeeRepository employeeRepository, 
+                ICurrentUserService currentUserService, 
+                IDateTimeHelper dateTimeHelper, 
+                ILogger<AddEmployeeCommand> logger
+            ) : base(employeeRepository, currentUserService, dateTimeHelper,logger)
         {
 
         }
@@ -26,10 +30,19 @@ namespace OvertimePolicies.Services.Tests.Employee.Commands
             // Arrange
             var mediatorMock = new Mock<IMediator>();
             var sut = new AddEmployeeCommandHandler(_employeeRepository, _currentUserService, _dateTimeHelper,_logger);
+            var employee = new Domain.Entities.Employee 
+            {
+                EmployeeId = 1234,
+                FirstName="Morteza",
+                LastName="Hasani",
+                EmploymentDate=DateTime.Now                
+            };
 
             // Act
+            var result = sut.Handle(new AddEmployeeCommand() { FirstName = "Morteza", LastName = "Hasani", EmploymentDate = DateTime.Now }, CancellationToken.None);
 
             // Assert
+            mediatorMock.Verify(m => m.Publish(It.Is<EmployeeAddedEvent>(x => x.Employee == employee), It.IsAny<CancellationToken>()), Times.Once);
         }
     }
 }
